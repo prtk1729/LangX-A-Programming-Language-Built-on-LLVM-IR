55da602fe733591ac54f9ab8549c6bf128e81741
prtk1729 <prateek.pani4243@gmail.com>
2025-06-10 01:36:50 +0530

    Added files

---
+++ eva-llvm/test.cpp
@@ -0,0 +1,7 @@
+/* cpp program to return 42
+    - Need to check how we can emit the IRs
+    - Need to check if we can use the IR and feed
+    duirectly to clang to get the binary file
+    - Run using llc <ll compiler>
+    - Run using lli <ll interp[reter]> */
+int main() { return 42; }
